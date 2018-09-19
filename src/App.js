e3476e4e0d6986461972fc8af16778f183a934a1
Carmen Owen <doctorsout@yahoo.com>
2018-09-19 11:06:11 -0400

    removed comments and unused code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-// import React from "react";
 import React, { Component } from "react";
-
 import Wrapper from "./components/Wrapper/Wrapper";
 import Grid from "./components/Grid/Grid";
-
-// const App = () => (
-//   <div>
-//     <Header />
-//     <Grid />
-//   </div>
-// );
-
-// export default App;
-
 
 
 class App extends Component {
   
-
-//   changeToClickedTrue = id => {
-    
-//   };
-
-  // Map over this.state.femmes and render a Femmes component for each femme object
   render() {
     return (
       <Wrapper>
-        
-          <Grid>
-            {/* {this.state.femmes.map(femme => (
-              <Femmes
-                clicked={this.changeToClickedTrue}
-                id={femme.id}
-                key={femme.id}
-                name={femme.name}
-                image={femme.image}
-              />
-            ))} */}
-          </Grid>
+          <Grid />
       </Wrapper>
     );
   }
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -74,17 +74,3 @@ class Grid extends Component {
 
 export default Grid;
     
-    // <div class="wrapper">
-    //     <div id="grid-item-1" class="grid-item">One</div>
-    //     <div id="grid-item-2" class="grid-item">Two </div>
-    //     <div id="grid-item-3" class="grid-item">Three</div>
-    //     <div id="grid-item-4" class="grid-item">Four</div>
-    //     <div id="grid-item-5" class="grid-item">Five</div>
-    //     <div id="grid-item-6" class="grid-item">Six</div>
-    //     <div id="grid-item-7" class="grid-item">Seven</div>
-    //     <div id="grid-item-8" class="grid-item">Eight</div>
-    //     <div id="grid-item-9" class="grid-item">Nine</div>
-    //     <div id="grid-item-10" class="grid-item">Ten</div>
-    //     <div id="grid-item-11" class="grid-item">Eleven</div>
-    //     <div id="grid-item-12" class="grid-item">Twelve</div>
-    // </div>
